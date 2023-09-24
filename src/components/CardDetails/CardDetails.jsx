785611ffaff37cbac68ecef45c55e06d1ee28f6e
shohaib1996 <khanshohaibhossain@gmail.com>
2023-09-25 00:17:58 +0600

    banner and home page done

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -7,12 +7,13 @@ const CardDetails = () => {
     const {id} = useParams()
     // console.log(id);
     const card = cards.find(card => card.id === id*1)
-    
+    const { image, background_color, category, title, text_color } = card;
+
     console.log(card)
     
     return (
         <div>
-            <h1>Card Details</h1>
+            
         </div>
     );
 };
