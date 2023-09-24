4415740ea09b9d9652c0c9450f09f1604274ae07
shohaib1996 <khanshohaibhossain@gmail.com>
2023-09-25 01:41:44 +0600

    Card section added

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -4,17 +4,42 @@ import { useLoaderData, useParams } from "react-router-dom";
 const CardDetails = () => {
     const cards = useLoaderData();
     // console.log(cards)
-    const {id} = useParams()
+    const { id } = useParams()
     // console.log(id);
-    const card = cards.find(card => card.id === id*1)
-    const { image, background_color, category, title, text_color } = card;
+    const card = cards.find(card => card.id === id * 1)
+    const { image, title, text_color, description, price } = card;
+    const backgroundColor = {
+        backgroundColor: text_color,
+    };
 
-    console.log(card)
-    
+    // console.log(card)
+    const handleDonate = () => {
+        const getStoredDonate = JSON.parse(localStorage.getItem('donation')) || [];
+        const isExit = getStoredDonate.find(card => card.id === id * 1)
+        if(!isExit){
+            getStoredDonate.push(card)
+            localStorage.setItem('donation', JSON.stringify(getStoredDonate))
+            return alert('item added')
+        }else{
+            return alert('duplicate item')
+        }
+    }
+
     return (
-        <div>
-            
+        <div className="max-w-screen-xl mx-auto mt-12 mb-12">
+            <div>
+                <div className="hero h-[80vh] relative " style={{ backgroundImage: `url(${image})`,backgroundSize: 'cover',backgroundPosition: 'center' }}>
+                    <div className="hero-overlay bg-opacity-60 absolute top-[75%] h-[25%] inset-0"></div>
+
+
+                    <button onClick={handleDonate} className="btn absolute top-[80%] right-[85%] text-white font-bold outline-none border-none" style={backgroundColor}>Donate ${price}</button>
+
+                </div>
+            </div>
+            <h1 className="text-3xl font-bold text-black mb-6 mt-20">{title}</h1>
+            <p className="text-justify text-black">{description}</p>
         </div>
+
     );
 };
 
